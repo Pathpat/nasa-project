0210641d575f6bb51fdb85037a701ee159529902
Pathpat <xristospatakias@gmail.com>
2023-09-05 11:10:11 +0300

    I list launches,put auto_increment and saveLaunch to mongoDb

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,28 +1,63 @@
-//const launches = require('./launches.mongo')
+const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
+
+const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launches = new Map();
 
-let latestFlightNumber = 100;
 
 const launch = {
     flightNumber: 100,
     mission:'Kepler exploration X',
     rocket: 'Explorer IS1',
     launchDate: new Date('December 27, 2030'),
-    target: 'kepler-442 b',
+    target: 'Kepler-442 b',
     customer: ['NASA','ZTM'],
     upcoming: true,
     success: true,
 }
 
-launches.set(launch.flightNumber, launch);
+
+saveLaunch(launch);
 
 function existsLaunchWithId(launchId) {
     return launches.has(launchId);
 }
 
-function getAllLaunches() {
-    return Array.from(launches.values());
+//Auto_Increment /(-flightNumber) means decrement order.
+async function getLatestFlightNumber() {
+    const latestLaunch = await launchesDatabase
+      .findOne()
+      .sort('-flightNumber');
+    
+    if (!latestLaunch) {
+        return DEFAULT_FLIGHT_NUMBER;
+    }
+    return latestLaunch.flightNumber;
+}
+
+//list launches.
+async function getAllLaunches() {
+    return await launchesDatabase
+    .find({},{
+        '_id': 0,
+        '__v':0
+    });
+}
+
+//save launches and create a strict rule  about the name of planets.
+async function saveLaunch(launch) {
+    const planet = await planets.findOne({
+        keplerName: launch.target,
+    });
+    if (!planet) {
+        throw new Error('No matching planet found')
+    }
+    await launchesDatabase.updateOne({
+        flightNumber: launch.flightNumber,
+    }, launch, {
+        upsert: true
+    });
 }
 
 function addNewLaunch(launch) {
