96623e78cee9dd9444f0e7b176bbe45673ebea56
Pathpat <xristospatakias@gmail.com>
2023-09-05 12:26:33 +0300

    Remake the abortlaunchbyid for mongodb

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -3,8 +3,6 @@ const planets = require('./planets.mongo');
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launches = new Map();
-
 
 const launch = {
     flightNumber: 100,
@@ -20,8 +18,10 @@ const launch = {
 
 saveLaunch(launch);
 
-function existsLaunchWithId(launchId) {
-    return launches.has(launchId);
+async function existsLaunchWithId(launchId) {
+    return await launchesDatabase.findOne({
+        flightNumber: launchId,
+    });
 }
 
 //Auto_Increment /(-flightNumber) means decrement order.
@@ -74,11 +74,14 @@ async function scheduleNewLaunch(launch) {
 }
 
 
-function abortLaunchById(launchId) {
-    const aborted =  launches.get(launchId);
-    aborted.upcoming = false;
-    aborted.success = false;
-    return aborted;
+async function abortLaunchById(launchId) {
+    const aborted = await launchesDatabase.updateOne({
+            flightNumber: launchId,
+        },{
+            upcoming: false,
+            success: false
+        });
+    return aborted.modifiedCount === 1;
 }
 
 module.exports = {
